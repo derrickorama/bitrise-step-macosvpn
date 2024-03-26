5eebc322a62ccf4c44c562fc9d022876243653d8
Derrick Gall <derrick.gall@zubie.com>
2024-03-26 12:30:20 -0500

    Added script and all inputs

--- step.sh
+++ step.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${vpn_endpoint}"
+sudo macosvpn create --l2tp Default --endpoint $vpn_endpoint --username $vpn_user_name --password $vpn_password --sharedsecret $vpn_shared_secret
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-# envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+networksetup -connectpppoeservice Default
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+sleep 5
+
+# One more time, to be sure
+networksetup -connectpppoeservice Default
+
+function isnt_connected () {
+    scutil --nc status "Default" | sed -n 1p | grep -qv Connected
+}
+
+function poll_until_connected () {
+    let loops=0 || true
+    let max_loops=50
+
+    while isnt_connected "Default"; do
+        sleep 1
+        let loops=$loops+1
+        [ $loops -gt $max_loops ] && break
+    done
+
+    [ $loops -le $max_loops ]
+}
+
+if poll_until_connected "Default"; then
+    echo "Connected to Default."
+    exit 0
+else
+    echo "Timed out while connecting to VPN."
+    exit 1
+fi
